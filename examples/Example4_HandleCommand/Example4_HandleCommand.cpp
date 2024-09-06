3212d8e9b4647082ba495eed01061c36398fcd8e
Geoffroy Arenou <geoarenou@gmail.com>
2024-09-06 17:36:24 +0200

    Add example : How to handle command from serial

---
+++ examples/Example4_HandleCommand/Example4_HandleCommand.cpp
@@ -0,0 +1,28 @@
+/*
+    Start the serial communication at default baud rate speed : 921600
+*/
+
+#include "ESP32_Helper.h"
+using namespace Printer;
+
+void setup(void)
+{
+    ESP32_Helper::Initialisation();
+}
+
+int cpt = 0;
+void loop(void)
+{    
+    // This will be print on Serial
+    println("Hello World ", cpt, " times !");
+    delay(1000);
+
+    if (ESP32_Helper::HasWaitingCommand())
+    {
+        Command command = ESP32_Helper::GetCommand();
+        if(command.cmd.startsWith("cpt++"))
+        {
+            cpt++;
+        }
+    }
+}
