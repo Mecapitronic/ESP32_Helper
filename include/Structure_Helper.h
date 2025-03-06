e01bad56cd01a5774afecc99242c67978ddc5512
Geoffroy Arenou <geoffroy.arenou@milton-innovation.com>
2025-03-06 15:13:16 +0100

    remove pop_front

--- include/Structure_Helper.h
+++ include/Structure_Helper.h
@@ -333,11 +333,4 @@ struct Command
         dataStr = "";
     }
 };
-
-template <typename T>
-void pop_front(std::vector<T> &vec)
-{
-    assert(!vec.empty());
-    vec.erase(vec.begin());
-}
 #endif
