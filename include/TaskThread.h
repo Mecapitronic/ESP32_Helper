15e515448202b9dd8f63e695dc99746ffab43f18
Geoffroy <geoarenou@gmail.com>
2025-04-23 18:14:12 +0200

    visual studio

--- include/TaskThread.h
+++ include/TaskThread.h
@@ -1,16 +1,31 @@
 #ifndef TASK_THREAD_H
 #define TASK_THREAD_H
 
+#ifdef _VISUAL_STUDIO
+#include <pthread.h>
+#define portBASE_TYPE int
+typedef portBASE_TYPE BaseType_t;
+typedef unsigned portBASE_TYPE UBaseType_t;
+typedef void (*TaskFunction_t)(void*);
+#else
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "ESP32_Helper.h"
+#endif
 
 class TaskThread
 {
 private:
+#ifdef _VISUAL_STUDIO
+     pthread_t pthread;
+     // TaskFunction_t _pvTaskCode;
+     void (*_pvTaskCode)(void*);
+     const char* _pcName;
+#else
     TaskHandle_t _task;
     TaskFunction_t _pvTaskCode;
     String _pcName;
+#endif
     bool debug = false;
 
     void task()
@@ -51,15 +66,20 @@ public:
             SERIAL_DEBUG.println(_pcName);
         }
 
-        /* Task function. */
-        /* name of task. */
-        /* Stack size of task */
-        /* parameter of the task */
-        /* priority of the task */
-        /* Task handle to keep track of created task */
-        /* pin task to core x */
-        xTaskCreatePinnedToCore(TaskThread::startTaskImpl, pcName, usStackDepth, this, uxPriority, &_task, xCoreID);
-    }
+#ifdef _VISUAL_STUDIO
+         bool ret = pthread_create(&pthread, NULL, startThread, this);
+         pthread_setname_np(pthread, pcName);
+#else
+         /* Task function. */
+         /* name of task. */
+         /* Stack size of task */
+         /* parameter of the task */
+         /* priority of the task */
+         /* Task handle to keep track of created task */
+         /* pin task to core x */
+         xTaskCreatePinnedToCore(TaskThread::startTaskImpl, pcName, usStackDepth, this, uxPriority, &_task, xCoreID);
+#endif
+     }
 
     ~TaskThread()
     {
@@ -73,9 +93,9 @@ public:
 
     static void DeleteTask(TaskHandle_t task) { vTaskDelete(task); }
 
-    // get stack for task 2
-    // unsigned int temp2 = uxTaskGetStackHighWaterMark(nullptr);
-    // SERIAL_DEBUG.print("task2="); SERIAL_DEBUG.println(temp2);
-    // log_i( "fDoTheHumidityThing high watermark %d",  uxTaskGetStackHighWaterMark( NULL ) );
+     // get stack for task 2
+     // unsigned int32_t temp2 = uxTaskGetStackHighWaterMark(nullptr);
+     // SERIAL_DEBUG.print("task2="); SERIAL_DEBUG.println(temp2);
+     // log_i( "fDoTheHumidityThing high watermark %d",  uxTaskGetStackHighWaterMark( NULL ) );
 };
 #endif
