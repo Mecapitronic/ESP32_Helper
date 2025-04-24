d33de03b45ce48596df8e69c761f89046e247d98
Geoffroy Arenou <geoffroy.arenou@milton-innovation.com>
2025-04-24 17:45:54 +0200

    review Timer and Task

--- include/TaskThread.h
+++ include/TaskThread.h
@@ -6,8 +6,9 @@
 #define portBASE_TYPE int
 typedef portBASE_TYPE BaseType_t;
 typedef unsigned portBASE_TYPE UBaseType_t;
-typedef void (*TaskFunction_t)(void*);
+
 typedef void* TaskHandle_t;
+typedef void (*TaskFunction_t)(void*);
 #else
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -16,18 +17,21 @@ typedef void* TaskHandle_t;
 
 class TaskThread
 {
-private:
+   private:
 #ifdef _VISUAL_STUDIO
-     pthread_t pthread;
-     void (*_pvTaskCode)(void*);
-     const char* _pcName;
+    pthread_t pthread;
+    TaskHandle_t _task;
+    void (*_pvTaskCode)(void*);
+    const char* _pcName;
+    bool debug = true;
 #else
     TaskHandle_t _task;
     TaskFunction_t _pvTaskCode;
     String _pcName;
-#endif
     bool debug = false;
+#endif
 
+#ifdef _VISUAL_STUDIO
     void task()
     {
         if (debug)
@@ -35,39 +39,32 @@ private:
             SERIAL_DEBUG.print("Calling Task : ");
             SERIAL_DEBUG.println(_pcName);
         }
-        _pvTaskCode(NULL);
+        _pvTaskCode(_task);
     }
 
-#ifdef _VISUAL_STUDIO
-     static void* startThread(void* _this)
-     {
-         pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
-         myprintf("Start thread "); myprintf(((TaskThread*)_this)->_pcName); myprintf("\n");
-         ((TaskThread*)_this)->task();
-         myprintf("End thread "); myprintf(((TaskThread*)_this)->_pcName); myprintf("\n");
-         return NULL;
-     }
-#else
     static void* startThread(void* _this)
-     {
-         ((TaskThread*)_this)->task();
-         return NULL;
-     }
+    {
+        TaskThread tt;
+        tt._pvTaskCode = ((TaskThread*)_this)->_pvTaskCode;
+        tt._pcName = ((TaskThread*)_this)->_pcName;
+
+        pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+        myprintf("Start thread ");
+        myprintf(tt._pcName);
+        myprintf("\n");
+        tt.task();
+        myprintf("End thread ");
+        myprintf(tt._pcName);
+        myprintf("\n");
+        return NULL;
+    }
 #endif
-     static void startTaskImpl(void* _this)
-     {
-         SERIAL_DEBUG.print("Impl Task : ");
-         SERIAL_DEBUG.println(((TaskThread*)_this)->_pcName);
-         ((TaskThread*)_this)->task();
-     }
 
-
-
-public:
+   public:
     TaskThread() {}
-
-    TaskThread(TaskFunction_t pvTaskCode, const char *const pcName, const uint32_t usStackDepth = 10000,
-               UBaseType_t uxPriority = 5, const BaseType_t xCoreID = 0) : _pvTaskCode(pvTaskCode), _pcName(pcName)
+    TaskThread(TaskFunction_t pvTaskCode, const char* const pcName, const uint32_t usStackDepth = 10000,
+               UBaseType_t uxPriority = 5, const BaseType_t xCoreID = 0)
+		: _pvTaskCode(pvTaskCode), _pcName(pcName), _task(0x00)
     {
         if (debug)
         {
@@ -76,20 +73,20 @@ public:
         }
 
 #ifdef _VISUAL_STUDIO
-         bool ret = pthread_create(&pthread, NULL, startThread, this);
-         pthread_setname_np(pthread, pcName);
+        bool ret = pthread_create(&pthread, NULL, startThread, this);
+        pthread_setname_np(pthread, pcName);
 #else
-         /* Task function. */
-         /* name of task. */
-         /* Stack size of task */
-         /* parameter of the task */
-         /* priority of the task */
-         /* Task handle to keep track of created task */
-         /* pin task to core x */
-         xTaskCreatePinnedToCore(TaskThread::startTaskImpl, pcName, usStackDepth, this, uxPriority, &_task, xCoreID);
+        /* Task function. */
+        /* name of task. */
+        /* Stack size of task */
+        /* parameter of the task */
+        /* priority of the task */
+        /* Task handle to keep track of created task */
+        /* pin task to core x */
+        xTaskCreatePinnedToCore(pvTaskCode, pcName, usStackDepth, this, uxPriority, &_task, xCoreID);
 #endif
-     }
-
+    }
+    /*
     ~TaskThread()
     {
         if (debug)
@@ -98,20 +95,15 @@ public:
             SERIAL_DEBUG.println(_pcName);
             SERIAL_DEBUG.println("Deleted task");
         }
-    }
+    }*/
 
-    static void DeleteTask(TaskHandle_t task)
+    static void DeleteTask(TaskHandle_t taskHandle)
     {
 #ifdef _VISUAL_STUDIO
-
+        pthread_exit(NULL);
 #else
-        vTaskDelete(task);
+        vTaskDelete(taskHandle);
 #endif
     }
-
-     // get stack for task 2
-     // unsigned int32_t temp2 = uxTaskGetStackHighWaterMark(nullptr);
-     // SERIAL_DEBUG.print("task2="); SERIAL_DEBUG.println(temp2);
-     // log_i( "fDoTheHumidityThing high watermark %d",  uxTaskGetStackHighWaterMark( NULL ) );
 };
 #endif
--- include/TimerThread.h
+++ include/TimerThread.h
@@ -3,36 +3,43 @@
 
 #ifdef _VISUAL_STUDIO
 #include <pthread.h>
-typedef void *TimerHandle_t;
+typedef void* TimerHandle_t;
 typedef void (*TimerCallbackFunction_t)(void*);
 #else
 #include "freertos/FreeRTOS.h"
 #include "freertos/timers.h"
+#include "ESP32_Helper.h"
 #endif
 
 class TimerThread
 {
-private:
+   private:
 #ifdef _VISUAL_STUDIO
-pthread_t pthread;
-TimerHandle_t _timer;
-void (*_callBack)(void*);
-const char* _pcName;
-TickType_t _period;
-bool _arret = false;
+    pthread_t pthread;
+    TimerHandle_t _timer;
+    void (*_callBack)(void*);
+    const char* _pcName;
+    TickType_t _period;
+    bool _arret = false;
+    bool debug = true;
 #else
     TimerHandle_t _timer;
     TimerCallbackFunction_t _callBack;
     String _pcName;
     TickType_t _period;
+    bool debug = false;
 #endif
-
 
 #ifdef _VISUAL_STUDIO
     void timer()
     {
         while (!_arret)
         {
+            if (debug)
+            {
+                SERIAL_DEBUG.print("Calling Timer : ");
+                SERIAL_DEBUG.println(_pcName);
+            }
             _callBack(_timer);
             EspClass::timerSleep((double)_period / 1000);
         }
@@ -57,63 +64,59 @@ bool _arret = false;
     }
 #endif
 
-public:
+   public:
     TimerThread() {}
-    TimerThread(TimerCallbackFunction_t callBack, const char* const pcName, TickType_t period, bool autoReload = true, void * const id = 0)
+    TimerThread(TimerCallbackFunction_t callBack, const char* const pcName, TickType_t period, bool autoReload = true,
+                void* const id = 0)
+        : _callBack(callBack), _pcName(pcName), _period(period), _timer(0x00)
     {
-        _callBack = callBack;
-        _pcName = pcName;
-        _period = period;
+        if (debug)
+        {
+            SERIAL_DEBUG.print("Creating Timer : ");
+            SERIAL_DEBUG.println(_pcName);
+        }
 
-        SERIAL_DEBUG.print("Creating Timer : ");
-        SERIAL_DEBUG.println(_pcName);
-        
 #ifdef _VISUAL_STUDIO
-    bool ret = pthread_create(&pthread, NULL, startTimer, this);
-    pthread_setname_np(pthread, pcName);
+        bool ret = pthread_create(&pthread, NULL, startTimer, this);
+        pthread_setname_np(pthread, pcName);
 #else
-_timer = xTimerCreate(pcName, period, autoReload, (void *)0, callBack);
+        _timer = xTimerCreate(pcName, period, autoReload, (void*)0, callBack);
 #endif
     }
-    
+    /*
     ~TimerThread()
     {
-        SERIAL_DEBUG.print("Deleting Timer : ");
-        SERIAL_DEBUG.println(_pcName);
-#ifdef _VISUAL_STUDIO
-        // bool cancel = pthread_cancel(pthread);
-        // bool join = pthread_join(pthread, NULL);
-#else
-        //xTimerDelete(_timer);
-        SERIAL_DEBUG.println("Deleted timer");
-#endif
-    }
-    
-    TickType_t Period()
-    {
-        return _period;
-    }
+        if (debug)
+        {
+            SERIAL_DEBUG.print("Deleting Timer : ");
+            SERIAL_DEBUG.println(_pcName);
+            SERIAL_DEBUG.println("Deleted timer");
+        }
+    }*/
+
+    TickType_t Period() { return _period; }
 
     void Start()
     {
-        while(_timer==0x00){}
+        while (_timer == 0x00)
+        {
+        }
         SERIAL_DEBUG.print("Starting Timer : ");
         SERIAL_DEBUG.println(_pcName);
-        #ifdef _VISUAL_STUDIO
-        #else
+#ifdef _VISUAL_STUDIO
+#else
         xTimerStart(_timer, portMAX_DELAY);
-        #endif
+#endif
     }
 
     void Stop()
     {
         SERIAL_DEBUG.print("Stopping Timer : ");
         SERIAL_DEBUG.println(_pcName);
-        #ifdef _VISUAL_STUDIO
-        #else
+#ifdef _VISUAL_STUDIO
+#else
         xTimerStop(_timer, portMAX_DELAY);
-        #endif
-
+#endif
     }
 };
 #endif
