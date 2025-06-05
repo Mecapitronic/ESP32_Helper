3f8c4732ffea395ec3ba1c8d60081218c12ce661
Geoffroy Arenou <geoarenou@gmail.com>
2025-06-05 13:06:20 +0200

    TimerThread

--- include/TimerThread.h
+++ include/TimerThread.h
@@ -1,24 +1,80 @@
 #ifndef TIMER_THREAD_H
 #define TIMER_THREAD_H
 
+#ifdef _VISUAL_STUDIO
+#include <pthread.h>
+typedef void *TimerHandle_t;
+typedef void (*TimerCallbackFunction_t)(void *);
+#else
+#include "ESP32_Helper.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/timers.h"
-#include "ESP32_Helper.h"
+#endif
 
 class TimerThread
 {
 private:
+#ifdef _VISUAL_STUDIO
+    pthread_t pthread;
+    TimerHandle_t _timer;
+    void (*_callBack)(void *);
+    const char *_pcName;
+    TickType_t _period;
+    bool _arret = false;
+#else
     TimerHandle_t _timer = nullptr;
     TimerCallbackFunction_t _callBack = nullptr;
     String _pcName = "";
     TickType_t _period = 0;
+#endif
     bool _enable = false;
     bool _isRunning = false;
+
+#ifdef _VISUAL_STUDIO
+    void timer()
+    {
+        while (!_arret)
+        {
+            if (debugPrint)
+            {
+                myprintf("Calling Timer : ");
+                myprintf(_pcName);
+                myprintf("\n");
+            }
+            _callBack(_timer);
+            EspClass::timerSleep((double)_period / 1000);
+        }
+    }
+
+    static void *startTimer(void *_this)
+    {
+        TimerThread tt;
+        tt._callBack = ((TimerThread *)_this)->_callBack;
+        tt._pcName = ((TimerThread *)_this)->_pcName;
+        tt._period = ((TimerThread *)_this)->_period;
+        tt._arret = ((TimerThread *)_this)->_arret;
+
+        pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+        myprintf("Start timer ");
+        myprintf(tt._pcName);
+        myprintf("\n");
+        tt.timer();
+        myprintf("End timer ");
+        myprintf(tt._pcName);
+        myprintf("\n");
+        return NULL;
+    }
+#endif
 
 public:
     bool debugPrint = true;
     TimerThread() = default;
-    TimerThread(TimerCallbackFunction_t callBack, const char *const pcName, TickType_t period, bool autoReload = true, void *const id = 0)
+
+    TimerThread(TimerCallbackFunction_t callBack,
+                const char *const pcName,
+                TickType_t period,
+                bool autoReload = true,
+                void *const id = 0)
         : _callBack(callBack), _pcName(pcName), _period(period)
     {
         if (debugPrint)
@@ -27,8 +83,12 @@ public:
             SERIAL_DEBUG.println(_pcName);
         }
 
+#ifdef _VISUAL_STUDIO
+        bool ret = pthread_create(&pthread, NULL, startTimer, this);
+        pthread_setname_np(pthread, pcName);
+        _timer = this;
+#else
         _timer = xTimerCreate(pcName, period, autoReload, (void *)0, _callBack);
-
         if (_timer == 0x00)
         {
             if (debugPrint)
@@ -38,6 +98,7 @@ public:
                 SERIAL_DEBUG.println(" not created");
             }
         }
+#endif
     }
 
     ~TimerThread()
@@ -64,7 +125,10 @@ public:
                 SERIAL_DEBUG.println(_pcName);
             }
             Enable();
+#ifdef _VISUAL_STUDIO
+#else
             xTimerStart(_timer, portMAX_DELAY);
+#endif
         }
         else
         {
@@ -86,7 +150,10 @@ public:
                 SERIAL_DEBUG.print("Stopping Timer : ");
                 SERIAL_DEBUG.println(_pcName);
             }
+#ifdef _VISUAL_STUDIO
+#else
             xTimerStop(_timer, portMAX_DELAY);
+#endif
         }
         else
         {
@@ -121,6 +188,7 @@ public:
             // Wait for the timer to be disabled by callback at the end of the callback
         }
     }
+
     void Enable()
     {
         if (debugPrint)
@@ -130,10 +198,12 @@ public:
         }
         _enable = true;
     }
+
     bool IsEnable()
     {
         return _enable == true;
     }
+
     bool Running(bool _run)
     {
         return _isRunning = _run;
