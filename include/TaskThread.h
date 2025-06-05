c198abee06a72203c1ef1276822190471b2ed65a
Geoffroy <geoarenou@gmail.com>
2025-06-05 13:06:20 +0200

    TaskThread

--- include/TaskThread.h
+++ include/TaskThread.h
@@ -1,20 +1,68 @@
 #ifndef TASK_THREAD_H
 #define TASK_THREAD_H
 
+#ifdef _VISUAL_STUDIO
+#include <pthread.h>
+#define portBASE_TYPE int
+typedef portBASE_TYPE BaseType_t;
+typedef unsigned portBASE_TYPE UBaseType_t;
+
+typedef void *TaskHandle_t;
+typedef void (*TaskFunction_t)(void *);
+#else
+#include "ESP32_Helper.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "ESP32_Helper.h"
+#endif
 
 class TaskThread
 {
 private:
+#ifdef _VISUAL_STUDIO
+    pthread_t pthread;
+    TaskHandle_t _task;
+    void (*_pvTaskCode)(void *);
+    const char *_pcName;
+#else
     TaskHandle_t _task = nullptr;
     TaskFunction_t _pvTaskCode = nullptr;
     String _pcName = "";
+#endif
+
+#ifdef _VISUAL_STUDIO
+    void task()
+    {
+        if (debugPrint)
+        {
+            myprintf("Calling Task : ");
+            myprintf(_pcName);
+            myprintf("\n");
+        }
+        _pvTaskCode(_task);
+    }
+
+    static void *startThread(void *_this)
+    {
+        TaskThread tt;
+        tt._pvTaskCode = ((TaskThread *)_this)->_pvTaskCode;
+        tt._pcName = ((TaskThread *)_this)->_pcName;
+
+        pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+        myprintf("Start thread ");
+        myprintf(tt._pcName);
+        myprintf("\n");
+        tt.task();
+        myprintf("End thread ");
+        myprintf(tt._pcName);
+        myprintf("\n");
+        return NULL;
+    }
+#endif
 
 public:
     bool debugPrint = false;
     TaskThread() = default;
+
     /**
      * @brief Launch a new Task Thread
      *
@@ -24,8 +72,12 @@ public:
      * @param uxPriority Priority, from High (20) to Low (0)
      * @param xCoreID Core affinity
      */
-    TaskThread(TaskFunction_t pvTaskCode, const char *const pcName, const uint32_t usStackDepth = 10000,
-               UBaseType_t uxPriority = 5, const BaseType_t xCoreID = 0) : _pvTaskCode(pvTaskCode), _pcName(pcName)
+    TaskThread(TaskFunction_t pvTaskCode,
+               const char *const pcName,
+               const uint32_t usStackDepth = 10000,
+               UBaseType_t uxPriority = 5,
+               const BaseType_t xCoreID = 0)
+        : _pvTaskCode(pvTaskCode), _pcName(pcName)
     {
         if (debugPrint)
         {
@@ -33,6 +85,10 @@ public:
             SERIAL_DEBUG.println(_pcName);
         }
 
+#ifdef _VISUAL_STUDIO
+        bool ret = pthread_create(&pthread, NULL, startThread, this);
+        pthread_setname_np(pthread, pcName);
+#else
         /* Task function. */
         /* name of task. */
         /* Stack size of task */
@@ -40,7 +96,9 @@ public:
         /* priority of the task */
         /* Task handle to keep track of created task */
         /* pin task to core x */
-        xTaskCreatePinnedToCore(pvTaskCode, pcName, usStackDepth, this, uxPriority, &_task, xCoreID);
+        xTaskCreatePinnedToCore(
+            pvTaskCode, pcName, usStackDepth, this, uxPriority, &_task, xCoreID);
+#endif
     }
 
     ~TaskThread()
@@ -52,6 +110,13 @@ public:
         }
     }
 
-    static void DeleteTask(TaskHandle_t task) { vTaskDelete(task); }
+    static void DeleteTask(TaskHandle_t task)
+    {
+#ifdef _VISUAL_STUDIO
+        pthread_exit(NULL);
+#else
+        vTaskDelete(task);
+#endif
+    }
 };
 #endif
